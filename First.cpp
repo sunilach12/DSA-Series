801af0209892a777b71f15a081a20a2f9cbfaa66
sunilach12 <sunilacharya338@gmail.com>
2026-08-15 17:50:52 +0545

    DSA Series  Day 1

---
+++ First.cpp
@@ -0,0 +1,29 @@
+// C++ is the case sencetive
+// First  programming to output Hello sunil
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  // cout << "Sunil Acharya !!" << endl; // we can also use the endl --> "\n"
+  // cout<<"Acharya","\n";
+  cout << "Sunil " << "Acharya" << endl;
+
+  // variables  and data types
+  // Container to store data
+
+  int age = 25;
+  char name = 's'; //  char datatype only store the 1 character
+  float money = 200.3;
+  bool isSafe = false;
+  double doublePrice = 10000.33;
+
+  cout << "int : " << age << endl;
+  cout << "SIze of Binary bit :" << sizeof(age) << endl; //  this size of tells use how many bit are in age
+  cout << "Char :" << name << endl;
+  cout << "Float :" << money << endl;
+  cout << "Boolen :" << isSafe << endl;
+  cout << "double :" << doublePrice << endl;
+
+  return 0;
+}
