e1bb700d5c0b0ce7947f3496802c758afc85ff8e
sunilach12 <sunilacharya338@gmail.com>
2026-08-16 23:43:08 +0545

    update First.cpp

--- First.cpp
+++ First.cpp
@@ -12,18 +12,26 @@ int main()
   // variables  and data types
   // Container to store data
 
-  int age = 25;
-  char name = 's'; //  char datatype only store the 1 character
-  float money = 200.3;
-  bool isSafe = false;
-  double doublePrice = 10000.33;
+  //   int age = 25;
+  //   char name = 's'; //  char datatype only store the 1 character
+  //   float money = 200.3;
+  //   bool isSafe = false;
+  //   double doublePrice = 10000.33;
+  //  cout << "Char :" << name << endl;
+  //   cout << "Float :" << money << endl;
+  //   cout << "Boolen :" << isSafe << endl;
+  //   cout << "double :" << doublePrice << endl;
+  //   cout << "int : " << age << endl;
+  //   cout << "SIze of Binary bit :" << sizeof(age) << endl; //  this size of tells use how many bit are in age
+  // Type casting :
 
-  cout << "int : " << age << endl;
-  cout << "SIze of Binary bit :" << sizeof(age) << endl; //  this size of tells use how many bit are in age
-  cout << "Char :" << name << endl;
-  cout << "Float :" << money << endl;
-  cout << "Boolen :" << isSafe << endl;
-  cout << "double :" << doublePrice << endl;
+  char grade = 'a'; // 97 from ascci
+  int value = grade;
+  cout << value << endl;
+
+  double price = 100.099;
+  int new_price = (int)price;
+  cout << new_price << endl;
 
   return 0;
 }
