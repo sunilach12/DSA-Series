ec88ce28c7fe88c4d5a66c057fe2baec8e0e730b
sunilach12 <sunilacharya338@gmail.com>
2026-08-16 23:34:07 +0545

    Input in C++

---
+++ input_in_cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int age;
+  // cout << age << endl;   // it print the random values which is called Garbage value .
+  cout << "Enter the age :";
+  cin >> age;
+  cout << "Your age is : " << age << endl;
+
+  double price;
+  cout << "Enter the Price money :";
+  cin >> price;
+  cout << " age of the girls :" << price << endl;
+  return 0;
+}
